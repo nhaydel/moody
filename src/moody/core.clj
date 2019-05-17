d0220720b9e5c748e3603dfb2286e098c92e2207
nhaydel <nhaydel@constantcontact.com>
2019-05-17 08:55:10 -0400

    Split reviews into sentences for greater accuracy

--- src/moody/core.clj
+++ src/moody/core.clj
@@ -1,20 +1,33 @@
 (ns moody.core
   (:require [moody.data_prep :as prep]
             [moody.analyze :as analyze]
-            [moody.tmdb_client :as client]))
+            [moody.tmdb_client :as client]
+            [clojure.string :as string]))
 
 (def default_training_file "resources/training.csv")
 (def default_test_file "resources/test.csv")
 (def default_sentiment_map {"0" :negative "1" :positive})
+(def punctuation_regex #"(?<=[.!?]|[.!?][\\'\"])(?<!e\.g\.|i\.e\.|vs\.|p\.m\.|a\.m\.|Mr\.|Mrs\.|Ms\.|St\.|Fig\.|fig\.|Jr\.|Dr\.|Prof\.|Sr\.|[A-Z]\.)\s+")
+(def bag (prep/train default_training_file default_sentiment_map 2))
+
+(defn get-score
+  "Gets the predicted percentage score of a movie by categorizing the reviews
+    by predicted sentiment, then returning the (number of positive reviews)/total reviews."
+  [title]
+  (let [movie_id (client/get-movie-id title)
+        reviews (map #(get % "content") (client/get-reviews movie_id))
+        sentences (reduce concat (map #(string/split % punctuation_regex) reviews))
+        results (map #(analyze/nb-classifier bag default_sentiment_map %) sentences)]
+    (/ (count(filter #(= :positive %) results)) (count results))
+    )
+  )
 
 (defn -main
-  "Optionally train a machine learning algorithm implementation, then take "
   [& args]
-  (def bag (prep/train default_training_file default_sentiment_map 1))
-  (println (analyze/nb_classifier bag "this is good" default_sentiment_map))
-  (def captain_marvel_id (client/get-movie-id "Captain Marvel"))
-  (println captain_marvel_id)
-  )
+  (if-not (empty? args)
+    (println (str (* 100 (float (get-score (string/join " " args)))) "%"))
+    (println ("Please supply a movie name."))
+    ))
 
 
 
--- test/moody/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns moody.core-test
-  (:require [clojure.test :refer :all]
-            [moody.core :refer :all]))
-
-(deftest a-test
-  (testing "CRAP"
-    (is (= 1 1))))
